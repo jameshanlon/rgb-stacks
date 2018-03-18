b04512c051a11a5b3706ab39d6c975609292a01b
Jamie Hanlon <jamie@graphcore.ai>
2018-03-18 09:27:30 +0000

    Update serial protocall to set all drivers at once

--- rgbstacks.py
+++ rgbstacks.py
@@ -22,83 +22,33 @@ print ("Serial on {}, baud rate = {}".format(usart.name, BAUD_RATE))
 #      usart.flush()
 #      count = (count + 1)
 
-green = 0
-blue = 0
-red = 0
+def set_colour(id, payload):
+    usart.write(bytearray([255]+payload))
+    usart.flush()
 
-sleep = 0.1
+def random_colour():
+    data = []
+    for i in range(13):
+        red   = random.randrange(255)
+        green = random.randrange(255)
+        blue  = random.randrange(255)
+        data += [red, green, blue]
+    set_colour(id, data)
 
-def set_colour(id, r, g, b):
-        print '{} ({}, {}, {})'.format(id, r, g, b)
-	usart.write(chr(255))
-	usart.write(chr(id))
-	usart.write(chr(r))
-	usart.write(chr(g))
-	usart.write(chr(b))
-	usart.flush()
-	#while (usart.inWaiting() == 0):
-	#  pass
-        received_byte = usart.read()
-        print 'ack {}'.format(ord(received_byte[0]))
+SLEEP = 3
 
-def random_colour(id):
-  red = random.randrange(256)
-  green = random.randrange(256)
-  blue = random.randrange(256)
-  set_colour(id, red, green, blue)
-
-#set_colour(0, 0, 0, 0)
-#set_colour(1, 0, 0, 0)
-#set_colour(2, 0, 0, 0)
-#set_colour(3, 0, 0, 0)
-
-count = 0
-
+set_colour(i, [0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0,
+               0, 0, 0])
 while True:
-    for i in range(1,13):
-        set_colour(i, 0, 0, 0)
-
-#while True:
-#	set_colour(0, 255, 0, 0)
-#	set_colour(1, 255, 0, 0)
-#	set_colour(2, 255, 0, 0)
-#	set_colour(3, 0, 0, 0)
-#	time.sleep(3)
-#	set_colour(0, 0, 255, 0)
-#	set_colour(1, 0, 255, 0)
-#	set_colour(2, 0, 255, 0)
-#	set_colour(3, 0, 0, 0)
-#	time.sleep(3)
-#	set_colour(0, 0, 0, 255)
-#	set_colour(1, 0, 0, 255)
-#	set_colour(2, 0, 0, 255)
-#	set_colour(3, 0, 0, 0)
-#	time.sleep(3)
-#	set_colour(0, 255, 255, 255)
-#	set_colour(1, 255, 255, 255)
-#	set_colour(2, 255, 255, 255)
-#	set_colour(3, 0, 0, 0)
-#	time.sleep(5)
-#	set_colour(0, 255, 255, 255)
-#	set_colour(1, 255, 255, 255)
-#	set_colour(2, 255, 255, 255)
-#	set_colour(3, 255, 255, 255)
-#	time.sleep(5)
-
-
-#while True:
-#  random_colour(0)
-#  random_colour(1)
-#  random_colour(2)
-#  #random_colour(3)
-#  time.sleep(1.5)
-
-#while True:
-#        print 'r:{} g:{} b:{}'.format(red, green, blue)
-#        set_colour(0, red, green, blue)
-#        set_colour(1, red, green, blue)
-#        set_colour(2, red, green, blue)
-#        #green = (green + 1) % 100
-#        #blue = (blue + 1) % 100
-#        red = (red + 1) % 100
-#        #time.sleep(0.01)
+    random_colour()
+    time.sleep(SLEEP)
