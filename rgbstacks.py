322e0d8c07a8b4373653b47bfaa684210ef86337
James Hanlon <pi@raspberrypi.(none)>
2018-04-05 19:36:49 +0000

    Tweaks for testing single packet frame

--- rgbstacks.py
+++ rgbstacks.py
@@ -9,46 +9,28 @@ usart = serial.Serial(port, BAUD_RATE)
 usart.flushInput()
 print ("Serial on {}, baud rate = {}".format(usart.name, BAUD_RATE))
 
-#usart.write(chr(0))
-#usart.flush()
-#count = 0
-#
-#while True:
-#   if(usart.inWaiting() > 0):
-#      received_byte = usart.read()
-#      byte_val = ord(received_byte[0])
-#      print "[{}] Received byte  {}".format(count, byte_val)
-#      usart.write(chr(byte_val))
-#      usart.flush()
-#      count = (count + 1)
-
-def set_colour(id, payload):
-    usart.write(bytearray([255]+payload))
+def set_colour(payload):
+    assert len(payload) == (3*12)
+    usart.write(bytearray([chr(255)]+payload))
     usart.flush()
+
+def off():
+    set_colour([chr(0)]*(12*3))
 
 def random_colour():
     data = []
-    for i in range(13):
+    for i in range(12):
         red   = random.randrange(255)
         green = random.randrange(255)
         blue  = random.randrange(255)
-        data += [red, green, blue]
-    set_colour(id, data)
+        data.append(chr(red))
+        data.append(chr(green))
+        data.append(chr(blue))
+    set_colour(data)
 
-SLEEP = 3
+SLEEP = 1.0
 
-set_colour(i, [0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0,
-               0, 0, 0])
+off()
 while True:
     random_colour()
     time.sleep(SLEEP)
